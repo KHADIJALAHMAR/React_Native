74e48f19dbf2712b2e2c6a54700f9ea3de76cc7e
khadija <khadijalahmar8@gmail.com>
2022-05-11 17:26:56 +0100

    Add Structure Project

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>To share a photo from your phone with a friend, just press the button below! khadija</Text>
     </View>
   );
 }
---
+++ src/components/general/navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View ,Text ,SafeAreaView} from "react-native"
+
+export default function NavbarGeneral(){
+    return(
+        <View>
+            <Text>
+                <h1>hello</h1>
+            </Text>
+        </View>
+    )
+}
