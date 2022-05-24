85eb36d20d2b3c5d03df191c402c00ba5a8db524
khadija <khadijalahmar8@gmail.com>
2022-05-24 09:54:58 +0100

    Create Folder Service and install package Axios

---
+++ src/navigation/main/ButtonNavigation.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+export default function ButtonNavigation() {
+  return (
+    <View>
+      <Text>ButtonNavigation</Text>
+    </View>
+  )
+} 
