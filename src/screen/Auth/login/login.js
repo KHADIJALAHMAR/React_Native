2e45730b5d5e9b5300608a05a69e374db4d97563
khadija <khadijalahmar8@gmail.com>
2022-05-25 09:42:13 +0100

    Create Store and SetRole SetIdD

--- src/screen/Auth/login/login.js
+++ src/screen/Auth/login/login.js
@@ -1,19 +1,39 @@
 import { View, Text ,StyleSheet,TextInput,ImageBackground,TouchableOpacity} from 'react-native';
-import React from 'react';
+import {React,useState} from 'react';
 import { Colors } from '../../../componentes/generel/contants';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { LinearGradient } from 'expo-linear-gradient';
 import style from "./style";
-
+import {login} from "../../../services/Auth";
+import jwtDecode from "jwt-decode";
 // import {PrimaryButton } from "../../../componentes/generel/button/button";
 
 export default function Login ({navigation})  {
+    const [user, setUser] = useState({
+        email: "",
+        password:"",
+    });
+    const [submitted, setSubmitted] = useState(false);
+
+    const handelSubmit = async()=>{
+        login(user.email,user.password).then((response)=>{
+            const user = jwtDecode(response.data.accessToken);
+            console.log(user);
+            // token = "token=" + JSON.stringify(respon se.data.accessToken)
+            // AsyncStorage.setItem()
+            console.log(response, token)
+            // if(user.role==="client"){
+            // navigate('/home')
+            // }
+            setSubmitted(true);
+        })
+        }
 
 
     return (
-      <View style={style.container}>
-           <View style={style.header}>
-              <ImageBackground style={{height: "100%",width:"100%" , position: 'absolute'}}  source={require('../../../../assets/style-4.png')}/>
+    <View style={style.container}>
+        <View style={style.header}>
+            <ImageBackground style={{height: "100%",width:"100%" , position: 'absolute'}}  source={require('../../../../assets/style-4.png')}/>
                 <Text  style={[style.text_header,{marginBottom: 80}]}>Welcome!</Text>
             </View>
                 <View style={style.footer}>
@@ -22,6 +42,7 @@ export default function Login ({navigation})  {
                   <FontAwesome name="envelope"  size={20} />
                         <TextInput 
                             placeholder="Your Email"
+                            onChangeText={email => setUser({...user, email: email})}
                             placeholderTextColor="#666666"
                             style={[style.textInput, {
                                 color:Colors.text
@@ -34,6 +55,7 @@ export default function Login ({navigation})  {
                         <TextInput 
                             placeholder="Password"
                             placeholderTextColor="#666666"
+                            onChangeText={password => setUser({...user, password: password})}
                             style={[style.textInput, {
                                 color:Colors.text
                             }]}
@@ -41,7 +63,7 @@ export default function Login ({navigation})  {
                 </View>
                 <TouchableOpacity
                     style={style.button}
-                    onPress={() => navigation.navigate('register')}
+                    onPress={handelSubmit}
                     >
                 <LinearGradient
                     colors={['#E2BB73' ,'#AC9261']}
@@ -55,4 +77,4 @@ export default function Login ({navigation})  {
                 </View>
       </View>
     );
-  };
+};
